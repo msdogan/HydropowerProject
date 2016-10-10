0d67ea88f91255419bb946d3b3a303704f08f6f9
Mustafa Dogan <msdogan@ucdavis.edu>
2016-10-10 14:49:29 -0700

    pumped_storage.py now creates price-duration

--- Pumped_Storage/Pumped_Storage.py
+++ Pumped_Storage/Pumped_Storage.py
@@ -4,7 +4,6 @@
 ##
 from __future__ import division
 import numpy as np
-import time 
 import matplotlib.pyplot as plt
 import scipy.stats as sp
 import pandas as pd
@@ -12,32 +11,51 @@ import seaborn as sns
 sns.set_style('whitegrid')
 
 # this function creates price-duration curves
-def dur_curve(p):
-    data = np.sort(p)
-    rank = sp.rankdata(data, method='average')
+def dur_curve(load, duration, time_period):
+    data_raw = [] # empty list to store temporary data
+    if duration == 'Monthly':
+        month = months.index(time_period) + 1
+        print(month)
+        for i in range(len(load)):
+            if load.Month[i] == month: # Unit is $/MWh
+                data_raw.append(load.Price[i])                
+    elif duration == 'Annual':
+        for i in range(len(load)):
+            if load.Year[i] == float(time_period): # Unit is $/MWh
+                data_raw.append(load.Price[i])                
+    elif duration == 'Daily': # does not work for now
+        y,m,d = time_period.split("-") # year, month, day
+        for i in range(len(load)):
+            if load.Year[i] == float(y):
+                if load.Month[i] == float(m):
+                    if load.Day[i] == float(d):
+                        data_raw.append(load.Price[i])                
+    else:
+        print('please define correct duration and/or time period')
+        
+    # after determining what duration and time period to use, create price-duration data
+    data = np.sort(data_raw) # sort from large to small
+    rank = sp.rankdata(data, method='average') # calculate the rank
     rank = rank[::-1] # non-exceedance prob. Comment out to get exceedance prob
-    prob = [100*(rank[i]/(len(data)+1)) for i in range(len(data))]
+    prob = [100*(rank[i]/(len(data)+1)) for i in range(len(data))] # frequency data
     
     plt.figure()
     plt.scatter(prob,data)
     plt.ylabel('Price')
     plt.xlabel('Duration (%)')
-    plt.title('Price-Duration Curve')
-    return
-
-# create moving average curves
-def moving_aver():
+    plt.ylim([0,max(data)+10])
+    plt.xlim([0,100])
+    plt.title('Hourly Price-Duration Curve for ' + str(time_period))
+    plt.show()
     return
 
 # Load Price data from OASIS (CAISO) http://oasis.caiso.com/mrioasis/logon.do
 name = '20160901_20161002_PRC_LMP_DAM_20161005_11_19_18_v1.csv'
 df = pd.read_csv(name, parse_dates=True)
-months = ['Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
-#print(months.index('Oct'))
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep','Oct', 'Nov', 'Dec']
 
-P = [[],[],[],[],[],[]]
-#P = np.empty([1,6])
-columns = ['Year', 'Month', 'Day', 'Start_Hour', 'End_Hour', 'Price']
+P = [[],[],[],[],[],[]] # empty list to store required data
+columns = ['Year', 'Month', 'Day', 'Start_Hour', 'End_Hour', 'Price'] # headers for data frame
 
 # We are only interested in , start time, end time and LMP
 for i in range(len(df)):
@@ -45,17 +63,16 @@ for i in range(len(df)):
         P[0].append(df.INTERVALSTARTTIME_GMT[i].split("T")[0].split("-")[0])
         P[1].append(df.INTERVALSTARTTIME_GMT[i].split("T")[0].split("-")[1])
         P[2].append(df.INTERVALSTARTTIME_GMT[i].split("T")[0].split("-")[2])
-        P[3].append(df.INTERVALSTARTTIME_GMT[i].split("T")[1].split("-")[0])
-        P[4].append(df.INTERVALENDTIME_GMT[i].split("T")[1].split("-")[0])
+        P[3].append(df.INTERVALSTARTTIME_GMT[i].split("T")[1].split("-")[0].split(":")[0])
+        P[4].append(df.INTERVALENDTIME_GMT[i].split("T")[1].split("-")[0].split(":")[0])
         P[5].append(df.MW[i])
  
-P = np.array(P)    
-Date_Price = pd.DataFrame(P.T, columns = columns)
+P = np.array(P) # convert list to numpy array    
+Date_Price = pd.DataFrame(P.T, columns = columns, dtype = 'float') # convert list to data frame
 
-print(dur_curve(P[5]))
-#plt.plot(P[5])
-#plt.ylabel('LMP ($/MWh)')
-#plt.title('Price Time-Series')
-#plt.show()
+# Examples of 'dur_curve' function use
+#print(dur_curve(Date_Price, 'Annual', '2016')) # annual example
+#print(dur_curve(Date_Price, 'Monthly', 'Sep')) # monthly example
+print(dur_curve(Date_Price, 'Daily', '2016-9-5')) # daily example ('year-month-day')
 
 
