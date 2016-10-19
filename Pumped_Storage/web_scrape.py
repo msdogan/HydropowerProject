dd13b227b37576aa42e40d6c9a70fb5d019df6dd
Mustafa Dogan <msdogan@ucdavis.edu>
2016-10-19 15:42:23 -0700

    finally working scraping module

--- Pumped_Storage/web_scrape.py
+++ Pumped_Storage/web_scrape.py
@@ -10,6 +10,7 @@ import requests, zipfile, StringIO
 
 example_url = 'http://oasis.caiso.com/oasisapi/SingleZip?queryname=PRC_LMP&startdatetime=20160919T00:00-0000&enddatetime=20160920T00:00-0000&version=1&market_run_id=DAM&grp_type=ALL_APNODES&resultformat=6'
 example_url2 = 'http://oasis.caiso.com/oasisapi/SingleZip?queryname=PRC_LMP&startdatetime=20130919T07:00-0000&enddatetime=20130920T07:00-0000&version=1&market_run_id=DAM&node=LAPLMG1_7_B2&resultformat=6'
+example_url3 = 'http://oasis.caiso.com/oasisapi/GroupZip?groupid=DAM_LMP_GRP&startdatetime=20130919T07:00-0000&version=1&resultformat=6' # group url - all nodes, one day
 
 # building a url - components
 queryname = 'PRC_LMP' # download locational marginal price
@@ -24,8 +25,8 @@ months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep','Oct', '
 
 # enter starting and ending year and month that you want to download
 # the code below downloads all hourly price data for months you specified
-s_year = 2015 # starting year
-e_year = 2016 # ending year
+s_year = 2010 # starting year
+e_year = 2010 # ending year
 s_month = 9 # starting month
 e_month = 9 # ending month
 
@@ -34,19 +35,22 @@ t = 0
 y = s_year
 m = s_month
 i = s_month
+ds = [1,10,20]
 while t <= ((e_year-s_year)*12 + (e_month-s_month)):
     if i < 10:
         cm = '0' + str(i)
     else:
         cm = i
-    startdatetime = str(str(y) + str(cm) + '01' + 'T00:00-0000') # start time
-    enddatetime = str(str(y) + str(cm) + str(n_days[months[i-1]]) + 'T00:00-0000') # end time
-#    url = api_name + 'queryname=' + queryname + '&' + 'startdatetime=' + startdatetime + '&' + 'enddatetime=' + enddatetime + '&' + 'version=1' + '&' +  'market_run_id=' + market_run_id + '&' + 'grp_type=' + grp_type + '&' + 'resultformat=' + resultformat
-    url = api_name + 'queryname=' + queryname + '&' + 'startdatetime=' + startdatetime + '&' + 'enddatetime=' + enddatetime + '&' + 'version=1' + '&' +  'market_run_id=' + market_run_id + '&' + 'node=' + node + '&' + 'resultformat=' + resultformat
-    print('now downloading: ' + startdatetime, enddatetime)    
-    r = requests.get(url, stream=True) # request price data
-    z = zipfile.ZipFile(StringIO.StringIO(r.content)) # url request returns a zip file
-    z.extractall('Z:\HydropowerProject\Pumped_Storage\Price_Data') # unzip files, you can also specify a directory  
+    de = [10,20,n_days[months[i-1]]]
+    for x,item in enumerate(de):
+        startdatetime = str(str(y) + str(cm) + str(ds[x]) + 'T00:00-0000') # start time
+        enddatetime = str(str(y) + str(cm) + str(item) + 'T00:00-0000') # end time
+        url_all = api_name + 'queryname=' + queryname + '&' + 'startdatetime=' + startdatetime + '&' + 'enddatetime=' + enddatetime + '&' + 'version=1' + '&' +  'market_run_id=' + market_run_id + '&' + 'grp_type=' + grp_type + '&' + 'resultformat=' + resultformat
+        url_single = api_name + 'queryname=' + queryname + '&' + 'startdatetime=' + startdatetime + '&' + 'enddatetime=' + enddatetime + '&' + 'version=1' + '&' +  'market_run_id=' + market_run_id + '&' + 'node=' + node + '&' + 'resultformat=' + resultformat
+        print('now downloading: ' + startdatetime, enddatetime)
+        r = requests.get(url_all, stream=True) # request price data
+        z = zipfile.ZipFile(StringIO.StringIO(r.content)) # url request returns a zip file
+        z.extractall('Z:\Price_Data') # unzip files, you can also specify a directory  
     if (m % 12) == 0: # happy new year!
         y += 1 # next year
         i = 0 # reset months
